c2242f0be4d8fef384d0aaa0a5cbd2b733a8825e
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2023-06-14 21:07:57 -0300

    updates

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
 import Card from "../UI/Card";
 import ExpenseDate from "../Expenses/ExpenseDate";
 import "../Expenses/ExpenseItem.css";
+import { useState } from "react";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log("Clicked!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">R$ {props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
