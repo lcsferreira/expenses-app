528d2f08f74ff4e94b9abd201f126c43a93a7e6d
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2023-09-27 15:16:22 -0300

    Exercise: Filter expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,32 @@
 import Card from "../UI/Card";
 import ExpenseItem from "../Expenses/ExpenseItem";
 import "../Expenses/Expenses.css";
+import ExpensesFilter from "../NewExpense/ExpensesFilter";
+import { useState } from "react";
 
 export default function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2023");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onFilterYear={filterChangeHandler}
         />
-      ))}
-    </Card>
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 }
