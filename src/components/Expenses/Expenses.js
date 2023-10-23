ef88ee25a76c43f54a936325a2b449d03c2472ac
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2023-10-23 13:51:54 -0300

    Computed / Derived State

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,10 +7,23 @@ import { useState } from "react";
 export default function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2023");
 
+  let filterInfoText = "2019, 2021 & 2022";
+
+  if (filteredYear === "2022") {
+    filterInfoText = "2019, 2021 & 2023";
+  } else if (filteredYear === "2021") {
+    filterInfoText = "2019, 2022 & 2023";
+  } else if (filteredYear === "2019") {
+    filterInfoText = "2021, 2022 & 2023";
+  } else {
+    filterInfoText = "2019, 2021 & 2022";
+  }
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     console.log(selectedYear);
   };
+
   return (
     <div>
       <Card className="expenses">
@@ -18,6 +31,9 @@ export default function Expenses(props) {
           selected={filteredYear}
           onFilterYear={filterChangeHandler}
         />
+        <p className="expenses-filter-info">
+          Data for years {filterInfoText} is hidden.
+        </p>
         {props.expenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
